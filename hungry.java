cdf6bd59035c369b373b384011d6fabcbc0948b0
Aarthi1803 <aarthimuthaiya99@gmail.com>
2020-10-11 11:53:21 +0530

    added hungry prompt

--- hungry.java
+++ hungry.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class hungry{
 public static void main(String args[]){
-System.out.println("I am hungry");
+
+Scanner scan=new Scanner(System.in);
+		System.out.println("Are you hungry?");
+		String message=scan.nextLine();
+		if(message.equalsIgnoreCase("yes"))
+		System.out.println("eat samosa");
+		else
+		System.out.println("do your homework");
+
 }
 }
